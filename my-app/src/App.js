37e1c2ea330a0331934b2a86d9444ad98c321e06
Odhran <mullano@tcd.ie>
2020-05-09 14:38:20 +0100

    created playlist obj, getplaylist now returns playlists in this form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
+
+function TrackObj(name, artist, length) {
+  this.name = name;
+  this.artist = artist;
+  this.length = length;
+}
+
+function PlaylistObj(name, length, tracksURL) {
+  this.name = name;
+  this.length = length;
+  this.tracksURL = tracksURL;
+  //this.tracks = tracks;
+}
 
 class App extends Component {
   constructor(){
@@ -15,6 +27,7 @@ class App extends Component {
     }
     console.log(params);
   }
+  
   getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
@@ -30,13 +43,23 @@ class App extends Component {
   getPlaylists() {
     spotifyApi.getUserPlaylists().then(
         function (data) {
-          console.log(data)
+          var playlistArray = [];
+          for(let i = 0; i < data.items.length; i++) {
+            var playlist = new PlaylistObj(data.items[i].name, data.items[i].tracks.total, data.items[i].tracks.href);
+            playlistArray[i] = playlist;
+            console.log(playlistArray[i]);
+          }
+          return playlistArray;
         },
         function (err) {
           console.error(err);
         }
     );
-}
+
+  }
+  getPlaylistTracks(PlaylistObj) {
+    
+  }
 
   render() {
     return (
